2df4879cea39df89863c8a2b0d634cdd1bf2a141
rimeorange <simwb@hanmail.net>
2026-07-15 14:23:17 +0900

    feat: entity add

---
+++ portfolio-project/src/main/kotlin/com/rime/portfolioproject/domain/entity/Link.kt
@@ -0,0 +1,25 @@
+package com.rime.portfolioproject.domain.entity
+
+import com.rime.portfolioproject.domain.BaseEntity
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity
+class Link (
+    name: String,
+    content: String,
+    isActive: Boolean,
+) : BaseEntity() {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "link_id")
+    var id: Long? = null
+
+    var name: String = name
+    var content: String = content
+    var isActive: Boolean = isActive
+}
